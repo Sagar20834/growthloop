28e2abcede4e946c22491006cb820b52ed89f858
Sagar Kumar sah Kanu <sagarsah20834@gmail.com>
2024-11-21 15:34:36 +0545

    aa

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,25 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    fs: {
+      allow: [
+        path.resolve(__dirname, "public"),
+        path.resolve(__dirname, "src"),
+        path.resolve(__dirname, "node_modules"),
+      ],
+    },
+    proxy: {
+      "/": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        secure: false,
+        // rewrite: path => path.replace(/^\/api/, ''), // If your backend is not expecting '/api' prefix
+      },
+    },
+  },
+});
