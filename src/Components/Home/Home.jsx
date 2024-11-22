68945a97925c85dac970f5a24c859de16004af2c
Sagar Kumar sah Kanu <sagarsah20834@gmail.com>
2024-11-22 10:18:38 +0545

    Edited

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import HomeCarousel from "./HomeCarousel";
 import HomePage from "./HomePage";
 import CompanySliderSection from "./CompanySliderSection";
 import IdeasandExecutionSection from "./IdeasandExecutionSection";
